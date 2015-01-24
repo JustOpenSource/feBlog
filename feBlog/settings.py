1d157ec3ebdd7c637113fca0c28e38912586709a
Troy Wolfe <troythewolfe@gmail.com>
2015-01-24 10:38:06 -0800

    Moving local setting before fabric so that the secret key can come via local settings

--- feBlog/settings.py
+++ feBlog/settings.py
@@ -309,30 +309,6 @@ OPTIONAL_APPS = (
     PACKAGE_NAME_GRAPPELLI,
 )
 
-###################
-# DEPLOY SETTINGS #
-###################
-
-# These settings are used by the default fabfile.py provided.
-# Check fabfile.py for defaults.
-
- FABRIC = {
-     "SSH_USER": "", # SSH username for host deploying to
-     "HOSTS": ALLOWED_HOSTS[:1], # List of hosts to deploy to (eg, first host)
-     "DOMAINS": ALLOWED_HOSTS, # Domains for public site
-     "REPO_URL": "ssh://hg@bitbucket.org/user/project", # Project's repo URL
-     "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
-     "PROJECT_NAME": "", # Unique identifier for project
-     "REQUIREMENTS_PATH": "requirements.txt", # Project's pip requirements
-     "GUNICORN_PORT": 8000, # Port gunicorn will listen on
-     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
-     "DB_PASS": "", # Live database password
-     "ADMIN_PASS": "", # Live admin user password
-     "SECRET_KEY": SECRET_KEY,
-     "NEVERCACHE_KEY": NEVERCACHE_KEY,
- }
-
-
 ##################
 # LOCAL SETTINGS #
 ##################
@@ -345,6 +321,29 @@ try:
 except ImportError as e:
     if "local_settings" not in str(e):
         raise e
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+FABRIC = {
+    "SSH_USER": "", # SSH username for host deploying to
+    "HOSTS": ALLOWED_HOSTS[:1], # List of hosts to deploy to (eg, first host)
+    "DOMAINS": ALLOWED_HOSTS, # Domains for public site
+    "REPO_URL": "ssh://hg@bitbucket.org/user/project", # Project's repo URL
+    "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
+    "PROJECT_NAME": "", # Unique identifier for project
+    "REQUIREMENTS_PATH": "requirements.txt", # Project's pip requirements
+    "GUNICORN_PORT": 8000, # Port gunicorn will listen on
+    "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
+    "DB_PASS": "", # Live database password
+    "ADMIN_PASS": "", # Live admin user password
+    "SECRET_KEY": SECRET_KEY,
+    "NEVERCACHE_KEY": NEVERCACHE_KEY,
+}
 
 
 ####################
